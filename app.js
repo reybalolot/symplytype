0e4d2ee21003e1a1e003277a36b212965417b922
balolot.johnrey <balolot.johnrey@gmail.com>
2024-08-14 00:31:35 +0800

    added main routes

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const express = require('express');
+import express from "express";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+var selectedMode;
+
+app.get('/', (req, res) => {
+    switch (selectedMode) {
+        case 'type':
+            res.render('typing');
+            break;
+        case 'keyboard':
+            res.render('keyboard');
+            break;
+        case 'fake':
+            res.render('fake');
+            break;
+        default:
+            res.render('menu')
+            break;
+    }
+});
+
+app.post('/mode', (req, res) => {
+    selectedMode = req.body.modes
+    res.redirect('/');
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on: ${port}`);
